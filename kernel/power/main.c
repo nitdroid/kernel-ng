9c60d9a364e1a485ea56bc11bac7076ee51779e9
Alexey Roslyakov <alexey.roslyakov@newsycat.com>
2012-03-17 19:38:39 +0400

    Update to power subsystem (wakelocks, etc)

--- kernel/power/main.c
+++ kernel/power/main.c
@@ -147,7 +147,11 @@ static ssize_t state_store(struct kobject *kobj, struct kobj_attribute *attr,
 			   const char *buf, size_t n)
 {
 #ifdef CONFIG_SUSPEND
+#ifdef CONFIG_EARLYSUSPEND
+	suspend_state_t state = PM_SUSPEND_ON;
+#else
 	suspend_state_t state = PM_SUSPEND_STANDBY;
+#endif
 	const char * const *s;
 #endif
 	char *p;
@@ -169,7 +173,14 @@ static ssize_t state_store(struct kobject *kobj, struct kobj_attribute *attr,
 			break;
 	}
 	if (state < PM_SUSPEND_MAX && *s)
+#ifdef CONFIG_EARLYSUSPEND
+		if (state == PM_SUSPEND_ON || valid_state(state)) {
+			error = 0;
+			request_suspend_state(state);
+		}
+#else
 		error = enter_state(state);
+#endif
 #endif
 
  Exit:
@@ -217,10 +228,9 @@ static struct attribute * g[] = {
 	&pm_test_attr.attr,
 #endif
 #ifdef CONFIG_USER_WAKELOCK
-    &wake_lock_attr.attr,
-    &wake_unlock_attr.attr,
+	&wake_lock_attr.attr,
+	&wake_unlock_attr.attr,
 #endif
-
 	NULL,
 };
 
--- kernel/power/power.h
+++ kernel/power/power.h
@@ -246,13 +246,13 @@ extern suspend_state_t requested_suspend_state;
 
 #ifdef CONFIG_USER_WAKELOCK
 ssize_t wake_lock_show(struct kobject *kobj, struct kobj_attribute *attr,
-                        char *buf);
+			char *buf);
 ssize_t wake_lock_store(struct kobject *kobj, struct kobj_attribute *attr,
-                        const char *buf, size_t n);
+			const char *buf, size_t n);
 ssize_t wake_unlock_show(struct kobject *kobj, struct kobj_attribute *attr,
-                        char *buf);
+			char *buf);
 ssize_t  wake_unlock_store(struct kobject *kobj, struct kobj_attribute *attr,
-                        const char *buf, size_t n);
+			const char *buf, size_t n);
 #endif
 
 #ifdef CONFIG_EARLYSUSPEND
